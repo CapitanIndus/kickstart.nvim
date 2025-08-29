6ad96e5a376b1c4be372c0f58359f477d6bb2383
Julian Frank <julian.da.frank@proton.me>
2025-08-29 12:07:12 +0000

    added python lsp
    added pylsp

--- init.lua
+++ init.lua
@@ -677,6 +677,7 @@ require('lazy').setup({
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         cssls = {},
+        pylsp = {},
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
