387e4c2e6964b6fafdb716807a78ed7c6c24f411
Julian Frank <julian_dirk_amade.frank@mailbox.tu-dresden.de>
2026-05-13 17:49:09 +0200

    Window hopping enabeld and disabled annoying hjkl

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,12 +14,16 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
 
 --Personal Mappings
-vim.keymap.set({ 'n', 'x' }, 'z', 'n', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'x' }, 'Z', 'N', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'x', 'o' }, 'n', 'h', opts)
-vim.keymap.set({ 'n', 'x', 'o' }, 'r', 'j', opts)
-vim.keymap.set({ 'n', 'x', 'o' }, 's', 'l', opts)
-vim.keymap.set({ 'n', 'x', 'o' }, 'l', 'k', opts)
+vim.keymap.set({ 'n', 'x', 'o' }, 'z', 'n', { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'x', 'o' }, 'Z', 'N', { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'x', 'o' }, 'n', 'h', { noremap = true, silent = true, desc = 'Left' })
+vim.keymap.set({ 'n', 'x', 'o' }, 'r', 'j', { noremap = true, silent = true, desc = 'Down' })
+vim.keymap.set({ 'n', 'x', 'o' }, 's', 'l', { noremap = true, silent = true, desc = 'Right' })
+vim.keymap.set({ 'n', 'x', 'o' }, 'l', 'k', { noremap = true, silent = true, desc = 'Up' })
+--Disable hjkl
+vim.keymap.set({ 'n', 'x', 'o' }, 'h', '<Nop>', { noremap = true, silent = true, desc = 'which_key_ignore' })
+vim.keymap.set({ 'n', 'x', 'o' }, 'j', '<Nop>', { noremap = true, silent = true, desc = 'which_key_ignore' })
+vim.keymap.set({ 'n', 'x', 'o' }, 'k', '<Nop>', { noremap = true, silent = true, desc = 'which_key_ignore' })
 
 -- Undo auf ä redo auf Ä
 vim.keymap.set('n', 'ä', 'u', { noremap = true, silent = true })
@@ -32,6 +36,12 @@ vim.keymap.set('n', 'U', 'I', { noremap = true, silent = true })
 -- Wortbewegungen
 vim.keymap.set({ 'n', 'x', 'o' }, 'e', 'w', { noremap = true, silent = true })
 vim.keymap.set({ 'n', 'x', 'o' }, 't', 'b', { noremap = true, silent = true })
+
+-- Change Window
+vim.keymap.set({ 'n' }, '<leader>ws', '<cmd>wincmd l<CR>', { noremap = true, silent = true, desc = 'Go to right window' })
+vim.keymap.set({ 'n' }, '<leader>wl', '<cmd>wincmd k<CR>', { noremap = true, silent = true, desc = 'Go to upper window' })
+vim.keymap.set({ 'n' }, '<leader>wr', '<cmd>wincmd j<CR>', { noremap = true, silent = true, desc = 'Go to down window' })
+vim.keymap.set({ 'n' }, '<leader>wn', '<cmd>wincmd h<CR>', { noremap = true, silent = true, desc = 'Go to left window' })
 
 -- Wortende (nur Normal & Visual, damit Textobjekte bleiben)
 vim.keymap.set({ 'n', 'x' }, 'i', 'e', { noremap = true, silent = true })
