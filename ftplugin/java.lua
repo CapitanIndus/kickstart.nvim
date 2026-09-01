bad0729551e563c810c5d22357d8726523350ead
Julian Frank <julian.da.frank@proton.me>
2026-09-01 10:01:56 +0200

    feat(java): install jdtls and enable Lombok support

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,12 +1,22 @@
 local home = os.getenv 'HOME'
 local workspace_dir = home .. '/.local/share/eclipse/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local lombok_jar = vim.fs.joinpath(vim.fn.stdpath 'data', 'mason', 'packages', 'jdtls', 'lombok.jar')
+
+local cmd = {
+  'jdtls',
+  '-data',
+  workspace_dir,
+}
+
+if vim.fn.filereadable(lombok_jar) == 1 then
+  -- Lombok muss bereits in der JVM von jdtls als Java-Agent geladen werden.
+  table.insert(cmd, 2, '--jvm-arg=-javaagent:' .. lombok_jar)
+else
+  vim.notify('jdtls: Lombok-Agent nicht gefunden: ' .. lombok_jar, vim.log.levels.WARN)
+end
 
 local config = {
-  cmd = {
-    'jdtls',
-    '-data',
-    workspace_dir,
-  },
+  cmd = cmd,
 
   root_dir = require('jdtls.setup').find_root { 'pom.xml', 'build.gradle', '.git' },
 
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -286,7 +286,7 @@ return {
 
     -- Alle definierten Server plus zusaetzliche Tools wie Stylua automatisch installieren.
     local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, { 'stylua' })
+    vim.list_extend(ensure_installed, { 'stylua', 'jdtls' })
 
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
